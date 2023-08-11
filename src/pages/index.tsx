9280ecf0d047b2bcbdb4477213a1bd6a14295c18
Bruno Charnock <bruno.ccharnock@gmail.com>
2023-08-11 18:12:37 -0300

    Criando a tab Contato

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,10 +36,10 @@ export default function Home(props: any) {
       var tablink = document.getElementById(tab.id + "link") as HTMLElement;
       if (tab.id === id) {
         tab.style.display = "grid";
-        tablink.className = "tablinks active";
+        tablink.className = "tablinks active text-white";
       } else {
         tab.style.display = "none";
-        tablink.className = "tablinks";
+        tablink.className = "tablinks text-black";
       }
     });
   }
@@ -105,8 +105,60 @@ export default function Home(props: any) {
         </div>
 
         <div id="contatoTab" className="contato-container tabcontent">
-          <div>
-            <h2 className="text-black text-center">CONTATO</h2>
+          <div className="w-full max-w-xs">
+            <h2 className="text-black text-xl">
+              Você pode usar este formulário para entrar em contato comigo
+            </h2>
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="name"
+                >
+                  Seu nome
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="name"
+                  type="text"
+                ></input>
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Seu email
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="text"
+                ></input>
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="message"
+                >
+                  Mensagem
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="message"
+                  type="text"
+                  placeholder="Digite sua mensagem"
+                ></input>
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                >
+                  Enviar
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </main>

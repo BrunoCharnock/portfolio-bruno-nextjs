96b45d6ae6e5f8b1dffb9370bfe41ecb7d302f6e
Bruno Cardoso Charnock <68870621+BrunoCharnock@users.noreply.github.com>
2023-08-27 02:52:16 +0000

    Compenentização e refatoramento de `projects.tsx`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,38 +4,20 @@ import Navbar from "@/pages/navbar";
 import LandingPage from "@/pages/landingpage";
 import { Inter } from "next/font/google";
 import { useEffect, useRef } from "react";
+import Projects from "./projects";
 const inter = Inter({ subsets: ["latin"] });
 
-export async function getServerSideProps(context: any) {
-  const res = await fetch("https://api.github.com/users/BrunoCharnock/repos");
-  const repo = await res.json();
-  return { props: { repo } };
-}
-
 export default function Home(props: any) {
-  const myRef = useRef();
-  useEffect(() => {
-    console.log(myRef.current);
-    const observer = new IntersectionObserver((entries) => {
-      const entry = entries[0];
-      console.log("entrry", entry);
-    });
-  }, []);
   return (
     <>
       <Navbar />
       <div className="body">
         <div className="wrapper">
           <LandingPage />
-          <Contact />
           <About />
+          <Projects />
+          <Contact />
         </div>
-        {/*
-        <div id="projetosTab" className="projects-container tabcontent">
-          <Projects project={props.repo} />
-        </div>
-
-       Fim Main */}
       </div>
     </>
   );
---
+++ src/pages/projects.tsx
@@ -0,0 +1,74 @@
+import { GetServerSideProps } from "next";
+import stringify from "postcss/lib/stringify";
+import { useEffect, useState } from "react";
+import styles from "@/styles/projects.module.css";
+import { useInView } from "react-intersection-observer";
+
+interface IProject {
+  id: number;
+  name: string;
+  html_url: string;
+  description: string;
+  language: string;
+}
+
+interface IProjectProps {
+  project: IProject;
+}
+
+export async function getServerSideProps(context: any) {
+  const res = await fetch("");
+  const repo = await res.json();
+  return { props: { repo } };
+}
+
+export default function Projects(props: any) {
+  const [projects, setProjects] = useState([]);
+  const { ref, inView, entry } = useInView();
+
+  const fetchUserData = () => {
+    fetch("https://api.github.com/users/BrunoCharnock/repos")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setProjects(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  return (
+    <>
+      <div id="projetosTab" className={styles.projectscontainer}>
+        {projects.map((repo: any) => {
+          return (
+            <div
+              ref={ref}
+              key={repo.id}
+              className={
+                inView
+                  ? styles.projectscontaineritemShown
+                  : styles.projectscontaineritemHidden
+              }
+            >
+              <div className="px-6 py-4">
+                <div className={styles.projectstitle}>
+                  <a title={repo.name} href={repo.html_url} target="_blank">
+                    {repo.name}
+                  </a>
+                </div>
+                <p className={styles.projectsdescription}>{repo.description}</p>
+              </div>
+              <div className={styles.projectstagcontainer}>
+                <span className={styles.projectstag}>{repo.language}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
